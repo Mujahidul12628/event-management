b26425ed17fd6051ca5202aa63745409faa8bd36
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-05-27 09:10:45 +0600

    Cart removed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,20 +6,6 @@ const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      <div className="shadow-xl card w-96 bg-base-100">
-        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-        <div className="card-body">
-          <h2 className="card-title">
-            Shoes!
-            <div className="badge badge-secondary">NEW</div>
-          </h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="justify-end card-actions">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
